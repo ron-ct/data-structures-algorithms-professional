369118f6a685224fd30078fba3d706aa0ebab4d1
ron-ct <ronkaranja@outlook.com>
2025-03-01 13:14:28 +0300

    priority queue methods

--- heap/priorityQueue.java
+++ heap/priorityQueue.java
@@ -1,5 +1,5 @@
 // Java program to demonstrate a priority queue
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class priorityQueue{
     public static void main(String[] args){
@@ -19,7 +19,33 @@ public class priorityQueue{
         //print top element again
         System.out.println(pq.peek());
 
+        // adding elements
+        //insertion order is not maintained by priority queue.
+        //elements are stored based on the priority order ascending by default
+        for(int i =0; i<10;i++){
+            pq.add(i);
+            pq.add(1);
 
+        }
+        System.out.println(pq);
+
+        //remove top element
+        pq.poll();
+
+        //remove specific element
+        pq.remove(7);
+
+        //check if queue has a specific element
+        System.out.println("Does the priority queue contain 3? " + pq.contains(3));
+
+        //check size of priority queue
+        System.out.println("Size of the queue: "+pq.size());
+
+        //remove all elements
+        //pq.clear();
+
+        //check if queue is empty
+        System.out.println("Queue is empty?"+ pq.isEmpty());
 
     }
 }
